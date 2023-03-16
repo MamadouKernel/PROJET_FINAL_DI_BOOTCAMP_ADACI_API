b9e01f35b48695311d620810db8c07204e2c994a
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-03-16 16:27:12 +0000

    Add files via upload

---
+++ src/main/java/com/dibootcampfinal/apiecocitoyens/models/Utilisateur.java
@@ -0,0 +1,54 @@
+package com.dibootcampfinal.apiecocitoyens.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "utilisateurs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Utilisateur{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Role role;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdDate = LocalDateTime.now();
+    }
+
+}
